9b68abf37b7cd2774561266d13fa64979f2b625a
Evan Kriminger <evankriminger@gmail.com>
2018-03-24 03:22:45 -0700

    wrote tfrecords writer

--- data/to_tfrecords.py
+++ data/to_tfrecords.py
@@ -1,21 +1,41 @@
 import struct
 import os
+import pickle
 import numpy as np
+import tensorflow as tf
 
 
 def cifar_reader(f):
   byte_str = f.read(3073) 
   if byte_str==b"":
-    return None
+    return None, None
   else:
     record_tuple = struct.unpack("3073B", byte_str)
     label = record_tuple[0]
     img = np.array(record_tuple[1:]).astype(np.uint8)
-    img = np.concatenate(np.split(img.reshape((32*3,32))[:,:,None], 3, axis=0), axis=2)
-    return img
+    #img = np.concatenate(np.split(img.reshape((32*3,32))[:,:,None], 3, axis=0), axis=2)
+    return img, label
+
 
 if __name__=="__main__":
+  
+  input_files = [fn for fn in os.listdir() if fn.endswith(".bin")]
+  output_files = [os.path.splitext(fn)[0]+".tfrecords" for fn in input_files]
 
-  with open(fn, "rb") as f:
-    while  
-    cifar_reader(f)  
+  for input_file, output_file in zip(input_files, output_files):
+    with open(input_file, "rb") as f:
+      with  tf.python_io.TFRecordWriter(output_file) as record_writer:
+        while True:
+          img, label = cifar_reader(f)  
+
+          if img is None:
+            break
+          
+          example = tf.train.Example(
+            features=tf.train.Features(
+              feature={
+                "image": tf.train.Feature(bytes_list=tf.train.BytesList(value=[img.tobytes()])),
+                "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[label]))
+              }))
+
+          record_writer.write(example.SerializeToString())
--- main.py
+++ main.py
@@ -3,6 +3,33 @@ from __future__ import division
 from __future__ import print_function
 
 import os
+import glob
 
 import numpy as np
 import tensorflow as tf
+
+
+def input_fn_gen(is_training=False):
+  def input_fn(params):
+    file_list = glob.glob("data/*.tfrecords")
+    dataset = tf.data.Dataset.list_files(file_list)
+  
+    if is_training:
+      dataset = dataset.shuffle(1024).repeat()
+
+    def process_dataset(file_name):
+      dataset = tf.data.TFRecordDataset(file_name, buffer_size=8*1024*1024)
+      return dataset
+
+    dataset = dataset.apply(
+      tf.contrib.data.parallel_interleave(
+        process_dataset, cycle_length=4, sloppy=True))
+    
+    dataset = dataset.shuffle(1024).prefetch(params["batch_size"])
+    return dataset
+
+  return input_fn
+
+
+if __name__=="__main__":
+  input_fn_gen({"batch_size":64})
